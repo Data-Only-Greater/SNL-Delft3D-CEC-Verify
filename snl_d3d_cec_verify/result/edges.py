83a9b3130721dd5095df1e5e6b548fbf0a2a3c65
Mathew Topper <damm_horse@yahoo.co.uk>
2021-12-01 12:57:53 +0000

    Finish Edges class
    Added edge normal vector calculation

--- snl_d3d_cec_verify/result/edges.py
+++ snl_d3d_cec_verify/result/edges.py
@@ -2,23 +2,19 @@
 
 from __future__ import annotations
 
-import os
 import collections
-from typing import Dict, Optional, Sequence, Tuple, TYPE_CHECKING, Union
-from pathlib import Path
+from typing import Dict, Optional, TYPE_CHECKING
 from dataclasses import dataclass, field
 
 import numpy as np
 import pandas as pd
 import geopandas as gpd
 import xarray as xr
-import matplotlib.pyplot as plt
 from shapely.geometry import LineString
 
-from ..types import Num, StrOrPath
+from ..types import StrOrPath
 
 if TYPE_CHECKING:
-    from ..cases import CaseStudy
     from shapely.geometry.base import BaseGeometry
 
 
@@ -28,9 +24,9 @@ class Edges:
     _t_steps: Dict[int, pd.Timestamp] = field(default_factory=dict,
                                               init=False,
                                               repr=False)
-    _frame: Optional[pd.DataFrame] = field(default=None,
-                                           init=False,
-                                           repr=False)
+    _frame: Optional[gpd.GeoDataFrame] = field(default=None,
+                                               init=False,
+                                               repr=False)
     
     def extract_k(self, t_step: int,
                         k: int,
@@ -74,7 +70,7 @@ class Edges:
 
 
 def map_to_edges_geoframe(map_path: StrOrPath,
-                       t_step: int = None) -> pd.DataFrame:
+                          t_step: int = None) -> gpd.GeoDataFrame:
     
     data = collections.defaultdict(list)
     
@@ -85,14 +81,37 @@ def map_to_edges_geoframe(map_path: StrOrPath,
         for iedge in ds.mesh2d_nEdges.values:
             
             points = []
+            two = (0, 1)
             
-            for inode in [0, 1]:
+            for inode in two:
                 index = ds.mesh2d_edge_nodes[iedge, inode] - 1
                 x = ds.mesh2d_node_x[index]
                 y = ds.mesh2d_node_y[index]
-                points.append((x, y))
+                p = np.array((x, y))
+                points.append(p)
             
             line = LineString(points)
+            linevec = points[1] - points[0]
+            normvec = np.array((-linevec[1], linevec[0]))
+            
+            points = []
+            
+            for iface in two:
+                
+                index = int(ds.mesh2d_edge_faces[iedge, iface]) - 1
+                
+                if index < 0:
+                    p = np.array(line.centroid)
+                else:
+                    x = ds.mesh2d_face_x[index]
+                    y = ds.mesh2d_face_y[index]
+                    p = np.array((x, y))
+                
+                points.append(p)
+            
+            facevec = points[1] - points[0]
+            normvec *= np.dot(facevec, normvec)
+            normvec /= np.linalg.norm(normvec)
             
             for k, ilayer in enumerate(ds.mesh2d_nLayers.values):
                 
@@ -102,64 +121,7 @@ def map_to_edges_geoframe(map_path: StrOrPath,
                 data["k"].append(k)
                 data["time"].append(time)
                 data["u1"].append(u1)
+                data["n0"].append(normvec[0])
+                data["n1"].append(normvec[1])
     
     return gpd.GeoDataFrame(data)
-
-
-def get_u1_intersection(ds, lines, transect, t=-1, layer=8):
-    
-    data = []
-    
-    for i, line in enumerate(lines):
-        
-        if not line.intersects(transect) or is_parallel(transect, line):
-            continue
-            
-        x = ds.mesh2d_edge_x[i].values.take(0)
-        y = ds.mesh2d_edge_x[i].values.take(0)
-        u1 = ds.mesh2d_u1[t, i, layer].values.take(0)
-        
-        data.append((x, y, u1))
-    
-    dtype = [('x', 'float'), ('y', 'float'), ('value', 'float')]
-    out = np.array(data, dtype=dtype)
-    out.sort(order=["x", "y"])
-    
-    return out
-
-
-def is_parallel(a, b, tol=1e-9):
-    
-    result = False
-    
-    a_vec = np.array((a.coords[1][0] - a.coords[0][0],
-                      a.coords[1][1] - a.coords[0][1]))
-    b_vec = np.array((b.coords[1][0] - b.coords[0][0],
-                      b.coords[1][1] - b.coords[0][1]))
-        
-    axb = np.cross(a_vec, b_vec)
-    if abs(axb) < tol: result = True
-        
-    return result
-
-
-def plot_intersect_x(u1, project_path, layer):
-    
-    fig_path = os.path.join(project_path, "centerline_edge_u1.png")
-    
-    plt.figure(figsize=(10,4))
-    plt.plot(u1["x"], u1["value"])
-    plt.axvline(6, color='k', ls="dashed")
-    plt.text(6.2,-0.5,'turbine',rotation=90)
-    plt.ylabel("u1 (m/s)")
-    plt.xlabel("x (m)")
-    title_str = ("Edge velocities intersecting turbine centerline (layer "
-                 "{})").format(layer)
-    plt.title(title_str)
-    plt.savefig(fig_path, bbox_inches='tight')
-    
-    msg_str = ("*** Plot created at '{}'\n"
-               "***").format(fig_path)
-    print(msg_str)
-    
-    return
