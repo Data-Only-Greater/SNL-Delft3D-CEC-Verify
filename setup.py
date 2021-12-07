a72da305eff902e6d2d4af3342cad86934e2f3f9
Mathew Topper <damm_horse@yahoo.co.uk>
2021-12-07 12:06:11 +0000

    Update to modern project layout

--- setup.py
+++ setup.py
@@ -1,20 +1,2 @@
 from setuptools import setup
-
-setup(name='SNL-Delft3D-CEC-Verify',
-      version='0.1',
-      description='SNL-Delft3D-CEC-Verify',
-      author='Mathew Topper',
-      author_email='mathew.topper@dataonlygreater.com',
-      packages=['snl_d3d_cec_verify'],
-      package_data={'snl_d3d_cec_verify': ['templates/fm/*.*']},
-      python_requires='>=3.9',
-      install_requires=['geopandas',
-                        'jinja2',
-                        'netCDF4',
-                        'numpy>=1.21',
-                        'pandas',
-                        'scipy',
-                        'shapely',
-                        'tabulate',
-                        'xarray'],
-      zip_safe=False)
+setup()
