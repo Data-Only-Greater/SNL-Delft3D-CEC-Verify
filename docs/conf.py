231c88158d21b331b45a4314043a44274b31377f
Mathew Topper <damm_horse@yahoo.co.uk>
2022-01-06 20:16:28 +0000

    Bump version in the docs as well

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = '2022, Mathew Topper'
 author = 'Mathew Topper'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
