d9fbe3cd1bcd7087ecd5bd7b5d3cc9d87bf32846
Mathew Topper <damm_horse@yahoo.co.uk>
2022-01-06 17:23:49 +0000

    Fix python version for docs build

--- docs/conf.py
+++ docs/conf.py
@@ -69,3 +69,9 @@ html_theme = 'insipid'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+
+# A list of paths that contain extra files not directly related to the 
+# documentation, such as robots.txt or .htaccess. Relative paths are taken as 
+# relative to the configuration directory. They are copied to the output 
+# directory. They will overwrite any existing file of the same name.
+html_extra_path = ["README.md"]
