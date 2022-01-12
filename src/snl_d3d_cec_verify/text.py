f9ab99274b5510124f5c63702c0a536d7bc2a8ba
Mathew Topper <damm_horse@yahoo.co.uk>
2022-01-12 11:23:52 +0000

    Upgrade Spinner to report any percentage found in given string

--- src/snl_d3d_cec_verify/text.py
+++ src/snl_d3d_cec_verify/text.py
@@ -1,23 +1,51 @@
 # -*- coding: utf-8 -*-
 
+import re
 import sys
 import itertools
+from typing import Optional
 
 
 class Spinner():
-    """A simple ASCII spinner which outputs to stdout
+    """An ASCII spinner which writes to stdout
+    
+    Running the spinner without input:
     
     >>> spin = Spinner()
     >>> for _ in range(4):
     ...     spin()
-    -\b/\b|\b\\\b
+    -\b \b/\b \b|\b \b\\\b \b
+    
+    If text passed to the spinner contains a percentage sign, the spinner
+    will display the percentage value:
+    
+    >>> for line in ["1.1%", "1.2%"]:
+    ...     spin(line)
+    1.1%\b\b\b\b    \b\b\b\b1.2%\b\b\b\b    \b\b\b\b
+    
+    .. automethod:: __call__
     
     """
     
     def __init__(self):
         self.spinner = itertools.cycle(['-', '/', '|', '\\'])
     
-    def __call__(self):
-        sys.stdout.write(next(self.spinner))
+    def __call__(self, text: Optional[str] = None):
+        """Increment the spinner and write to stdout
+        
+        :param text: text to parse for percentage sign
+        
+        """
+        
+        out = None
+        
+        if text is not None:
+            percentage = re.search(r'\d+\.\d+(?=%)', text)
+            if percentage is not None:
+                out = percentage.group() + "%"
+        
+        if out is None: out = next(self.spinner)
+        
+        sys.stdout.write(out)
         sys.stdout.flush()
-        sys.stdout.write('\b')
+        sys.stdout.write('\b' * len(out) + ' ' * len(out) + '\b' * len(out))
--- tests/test_text.py
+++ tests/test_text.py
@@ -14,12 +14,34 @@ def test_spinner(mocker):
         spin()
     
     expected_args = [mocker.call('-'),
-                     mocker.call('\x08'),
+                     mocker.call('\x08 \x08'),
                      mocker.call('/'),
-                     mocker.call('\x08'),
+                     mocker.call('\x08 \x08'),
                      mocker.call('|'),
-                     mocker.call('\x08'),
+                     mocker.call('\x08 \x08'),
                      mocker.call('\\'),
-                     mocker.call('\x08')]
+                     mocker.call('\x08 \x08')]
+    
+    assert spy_write.call_args_list == expected_args
+
+
+def test_spinner_text(mocker):
+    
+    from snl_d3d_cec_verify.text import sys
+    
+    spy_write = mocker.spy(sys.stdout, 'write')
+    spin = Spinner()
+    
+    for line in ["1.1%", "1.2%", "mock", "mock"]:
+        spin(line)
+    
+    expected_args = [mocker.call('1.1%'),
+                     mocker.call('\x08\x08\x08\x08    \x08\x08\x08\x08'),
+                     mocker.call('1.2%'),
+                     mocker.call('\x08\x08\x08\x08    \x08\x08\x08\x08'),
+                     mocker.call('-'),
+                     mocker.call('\x08 \x08'),
+                     mocker.call('/'),
+                     mocker.call('\x08 \x08')]
     
     assert spy_write.call_args_list == expected_args
