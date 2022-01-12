e4846de70529ab33dba769d3267524f5ca554e46
Mathew Topper <damm_horse@yahoo.co.uk>
2022-01-12 12:06:49 +0000

    Fix template test on Linux

--- tests/test_template.py
+++ tests/test_template.py
@@ -14,7 +14,7 @@ def test_package_fm_template_path():
     assert str(expected) in str(result)
 
 
-def test_template_call(mocker):
+def test_template_call(mocker, tmp_path):
     
     mock_copy = mocker.patch("snl_d3d_cec_verify.template.copy",
                              autospec=True)
@@ -22,7 +22,6 @@ def test_template_call(mocker):
                     "snl_d3d_cec_verify.template.write_gridfm_rectangle",
                     autospec=True)
     
-    template_path = "mock_template"
     exist_ok = True
     case = CaseStudy()
     project_path = "mock_template"
@@ -32,7 +31,7 @@ def test_template_call(mocker):
                                            if field not in excluded_fields]
     expected_net_path = Path(project_path) / "input" / "FlowFM_net.nc"
     
-    template = Template(template_path, exist_ok)
+    template = Template(tmp_path, exist_ok)
     template(case, project_path)
     
     mock_copy.assert_called()
