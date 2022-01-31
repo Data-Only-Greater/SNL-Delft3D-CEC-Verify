92f3ba9aca5f3df475b96f4d77f58aba10a143da
semantic-release <semantic-release>
2022-01-31 15:29:22 +0000

    0.4.3
    Automatically generated by python-semantic-release

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = '2022, Mathew Topper'
 author = 'Mathew Topper'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.4.3'
 
 
 # -- General configuration ---------------------------------------------------
