a39bbceda4359f3b942e2b0321c9f4d04be5fd64
Mathew Topper <damm_horse@yahoo.co.uk>
2022-02-18 17:09:04 +0000

    Add automatic detection of model type in `Runner` classes (#30)
    This PR add automatic detection of the model type (and relative file locations) to the `Runner` and `LiveRunner` classes.
    
    Other changes are as follows:
    
    * Removed the `relative_input_parts` argument to the `Runner` and `LiveRunner` classes, as the relative path to the model file location is now calculated automatically.
    * Added the `run_dflow2d3d` function to the `runner` module to call a structured Delft3D model using the script of the same name.
    * Added the `model_file` argument to the `run_dflowfm` to explicitly give the name (not including the path) of the .mdu file to be run. This allows an .mdu model file to have any name.
    * Updated the `MycekStudy` docstring to be aligned with the updated `CaseStudy` docstring.

--- src/snl_d3d_cec_verify/runner.py
+++ src/snl_d3d_cec_verify/runner.py
@@ -5,23 +5,26 @@ from __future__ import annotations
 import os
 import platform
 import subprocess
+from abc import ABC, abstractmethod
 from queue import Queue
-from typing import Iterator, IO, List, Optional
+from typing import Iterator, IO, Optional
 from pathlib import Path
 from threading import Thread
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 from .types import StrOrPath
 from ._docs import docstringtemplate
 
-__all__ = ["run_dflowfm"]
+__all__ = ["run_dflowfm", "run_dflow2d3d"]
 
 
 @docstringtemplate
 @dataclass
 class Runner:
-    """A wrapper around the :func:`.run_dflowfm` function to allow reuse of
-    settings across many Delft3D projects.
+    """A class for running Delft3D models which allow reuse of settings across 
+    multiple projects. Automatically detects if the model uses a flexible or
+    structured mesh and then calls the appropriate function from the 
+    :mod:`.runner` package.
     
     Call the Runner object with the project path to execute the Delft3D model
     
@@ -38,10 +41,6 @@ class Runner:
         {omp_num_threads}
     :param show_stdout: show Delft3D logging to stdout in console, defaults
         to {show_stdout}
-    :param relative_input_parts: list of components representing the
-        relative path to folder containing the delft3D model files, from the
-        project folder. Set to None to use given path directly. Defaults to
-        :code:`["input"]`
     
     .. automethod:: __call__
     
@@ -53,12 +52,6 @@ class Runner:
     omp_num_threads: int = 1  #: The number of CPU threads to use
     show_stdout: bool = False #: show Delft3D logging to stdout in console
     
-    #: list of components representing the relative path to folder containing
-    #: the delft3D model files, from the project folder. Set to None to given
-    #: path directly
-    relative_input_parts: Optional[List[str]] = field(
-                                            default_factory=lambda: ["input"])
-    
     def __call__(self, project_path: StrOrPath):
         """
         Run a simulation, given a prepared model.
@@ -67,23 +60,16 @@ class Runner:
         
         :raises OSError: if function is called on an unsupported operating
             system
-        :raises FileNotFoundError: if the Delft3D entry point or model folder
-            could not be found
+        :raises FileNotFoundError: if the Delft3D entry point or model file
+            could not be found (or is not unique)
         :raises RuntimeError: if the Delft3D simulation outputs to stderr, for
             any reason
         
         """
         
-        if self.relative_input_parts is None:
-            relative_input_parts = []
-        else:
-            relative_input_parts = self.relative_input_parts
-        
-        model_path = Path(project_path).joinpath(*relative_input_parts)
-        
-        sp = run_dflowfm(self.d3d_bin_path,
-                         model_path,
-                         self.omp_num_threads)
+        sp = _run_model(project_path,
+                        self.d3d_bin_path,
+                        self.omp_num_threads)
         
         out, err = sp.communicate()
         
@@ -98,8 +84,10 @@ class Runner:
 @docstringtemplate
 @dataclass
 class LiveRunner:
-    """A wrapper around the :func:`.run_dflowfm` function to allow reuse of
-    settings across many Delft3D projects with real time output.
+    """A class for running Delft3D models which allow reuse of settings across 
+    multiple projects with real time output. Automatically detects if the 
+    model uses a flexible or structured mesh and then calls the appropriate 
+    function from the :mod:`.runner` package.
     
     Call the LiveRunner object with the project path to execute the Delft3D
     model and read the output line by line, like a generator
@@ -116,10 +104,6 @@ class LiveRunner:
         Delft3D
     :param omp_num_threads: The number of CPU threads to use, defaults to
         {omp_num_threads}
-    :param relative_input_parts: list of components representing the
-        relative path to folder containing the delft3D model files, from the
-        project folder. Set to None to use given path directly. Defaults to
-        :code:`["input"]`
     
     .. automethod:: __call__
     
@@ -130,12 +114,6 @@ class LiveRunner:
     
     omp_num_threads: int = 1  #: The number of CPU threads to use
     
-    #: list of components representing the relative path to folder containing
-    #: the delft3D model files, from the project folder. Set to None to given
-    #: path directly
-    relative_input_parts: Optional[List[str]] = field(
-                                            default_factory=lambda: ["input"])
-    
     def __call__(self, project_path: StrOrPath) -> Iterator[str]:
         """
         Run a simulation, given a prepared model, and yield stdout and stdin
@@ -145,23 +123,16 @@ class LiveRunner:
         
         :raises OSError: if function is called on an unsupported operating
             system
-        :raises FileNotFoundError: if the Delft3D entry point or model folder
-            could not be found
+        :raises FileNotFoundError: if the Delft3D entry point or model file
+            could not be found (or is not unique)
         :raises RuntimeError: if the Delft3D simulation outputs to stderr, for
             any reason
         
         """
         
-        if self.relative_input_parts is None:
-            relative_input_parts = []
-        else:
-            relative_input_parts = self.relative_input_parts
-        
-        model_path = Path(project_path).joinpath(*relative_input_parts)
-        
-        sp = run_dflowfm(self.d3d_bin_path,
-                         model_path,
-                         self.omp_num_threads)
+        sp = _run_model(project_path,
+                        self.d3d_bin_path,
+                        self.omp_num_threads)
         
         q: Queue = Queue()
         Thread(target=_pipe_reader, args=[sp.stderr, q]).start()
@@ -189,9 +160,107 @@ class LiveRunner:
             raise RuntimeError("Delft3D simulation failure")
 
 
+def _run_model(project_path: StrOrPath,
+               d3d_bin_path: StrOrPath,
+               omp_num_threads: int = 1) -> subprocess.Popen:
+    
+    model_runner_classes = [_FMModelRunner,
+                            _StructuredModelRunner]
+    model_runner = None
+    
+    for ModelRunner in model_runner_classes:
+        test_runner = ModelRunner(project_path)
+        if test_runner.is_model():
+            model_runner = test_runner
+            break
+    
+    if model_runner is None:
+        msg = "No valid model files detected"
+        raise FileNotFoundError(msg)
+    
+    return model_runner.run_model(d3d_bin_path,
+                                  omp_num_threads)
+
+
+@dataclass(frozen=True)
+class _BaseModelRunnerDataclassMixin:
+    project_path: StrOrPath
+
+
+class _BaseModelRunner(ABC, _BaseModelRunnerDataclassMixin):
+    
+    def is_model(self) -> bool:
+        model_path = self._get_model_path()
+        if model_path is None: return False
+        return True
+    
+    @abstractmethod
+    def _get_model_path(self) -> Optional[Path]:
+        pass    # pragma: no cover
+    
+    @abstractmethod
+    def run_model(self, d3d_bin_path: StrOrPath,
+                        omp_num_threads: int = 1) -> subprocess.Popen:
+        pass    # pragma: no cover
+
+
+class _FMModelRunner(_BaseModelRunner):
+    
+    def _get_model_path(self) -> Optional[Path]:
+        return _find_path(self.project_path, ".mdu")
+    
+    def run_model(self, d3d_bin_path: StrOrPath,
+                        omp_num_threads: int = 1) -> subprocess.Popen:
+        
+        model_path = self._get_model_path()
+        
+        if model_path is None:
+            raise FileNotFoundError("No .mdu file detected")
+        
+        return run_dflowfm(d3d_bin_path,
+                           model_path.parent,
+                           model_path.name,
+                           omp_num_threads)
+
+
+class _StructuredModelRunner(_BaseModelRunner):
+    
+    def _get_model_path(self) -> Optional[Path]:
+        return _find_path(self.project_path, ".xml", "config_d_hydro")
+    
+    def run_model(self, d3d_bin_path: StrOrPath,
+                        omp_num_threads: int = 1) -> subprocess.Popen:
+        
+        model_path = self._get_model_path()
+        
+        if model_path is None:
+            raise FileNotFoundError("No config_d_hydro.xml file detected")
+        
+        return run_dflow2d3d(d3d_bin_path,
+                             model_path.parent,
+                             omp_num_threads)
+
+
+def _find_path(project_path: StrOrPath,
+               ext: str,
+               file_root: Optional[str] = None) -> Optional[Path]:
+    
+    if file_root is None: file_root = "*"
+    files = list(Path(project_path).glob(f"**/{file_root}{ext}"))
+    
+    if len(files) > 1:
+        msg = f"Multiple files detected with signature '{file_root}{ext}'"
+        raise FileNotFoundError(msg)
+    
+    if not files: return None
+    
+    return files[0]
+
+
 @docstringtemplate
 def run_dflowfm(d3d_bin_path: StrOrPath,
                 model_path: StrOrPath,
+                model_file: str,
                 omp_num_threads: int = 1) -> subprocess.Popen:
     """Run a Delft3D flexible mesh simulation, given an existing Delft3D
     installation and a prepared model.
@@ -209,10 +278,54 @@ def run_dflowfm(d3d_bin_path: StrOrPath,
         could not be found
     :raises RuntimeError: if the Delft3D simulation outputs to stderr, for any
         reason
-
+    
     """
     
-    dflowfm_entry_point = _get_dflowfm_entry_point(d3d_bin_path)
+    return _run_script("dflowfm",
+                       d3d_bin_path,
+                       model_path,
+                       omp_num_threads,
+                       model_file)
+
+
+@docstringtemplate
+def run_dflow2d3d(d3d_bin_path: StrOrPath,
+                  model_path: StrOrPath,
+                  omp_num_threads: int = 1) -> subprocess.Popen:
+    """Run a Delft3D structured mesh simulation, given an existing Delft3D
+    installation and a prepared model.
+    
+    Currently only available for Windows and Linux.
+    
+    :param d3d_bin_path: path to the ``bin`` folder generated when compiling
+        Delft3D
+    :param model_path: path to folder containing the Delft3D model files
+    :param omp_num_threads: The number of CPU threads to use, defaults to
+        {omp_num_threads}
+    
+    :raises OSError: if function is called on an unsupported operating system
+    :raises FileNotFoundError: if the Delft3D entry point or model folder
+        could not be found
+    :raises RuntimeError: if the Delft3D simulation outputs to stderr, for any
+        reason
+    
+    """
+    
+    return _run_script("dflow2d3d",
+                       d3d_bin_path,
+                       model_path,
+                       omp_num_threads)
+
+
+def _run_script(name: str,
+                d3d_bin_path: StrOrPath,
+                model_path: StrOrPath,
+                omp_num_threads: int = 1,
+                *args: str) -> subprocess.Popen:
+    
+    if args is None: args = []
+    
+    entry_point = _get_entry_point(d3d_bin_path, name)
     model_path = Path(model_path)
     
     if not model_path.is_dir():
@@ -221,7 +334,9 @@ def run_dflowfm(d3d_bin_path: StrOrPath,
     
     env = dict(os.environ)
     env['OMP_NUM_THREADS'] = f"{omp_num_threads}"
-    sp = subprocess.Popen([dflowfm_entry_point, "FlowFM.mdu"],
+    popen_args = [str(entry_point.resolve())] + list(args)
+    
+    sp = subprocess.Popen(popen_args,
                           stdout=subprocess.PIPE,
                           stderr=subprocess.PIPE,
                           cwd=model_path,
@@ -230,26 +345,28 @@ def run_dflowfm(d3d_bin_path: StrOrPath,
     return sp
 
 
-def _get_dflowfm_entry_point(d3d_bin_path: StrOrPath) -> Path:
+def _get_entry_point(d3d_bin_path: StrOrPath,
+                     name: str) -> Path:
     
     os_name = platform.system()
     
-    # TODO: convert to match-case when 3.10 is supported by deps
     if os_name == 'Windows':
-        dflowfm_entry_point = Path(d3d_bin_path).joinpath("x64",
-                                                          "dflowfm",
-                                                          "scripts",
-                                                          "run_dflowfm.bat")
+        script = "run_" + name + '.bat'
+        entry_point = Path(d3d_bin_path).joinpath("x64",
+                                                  name,
+                                                  "scripts",
+                                                  script)
     elif os_name == 'Linux':
-        dflowfm_entry_point = Path(d3d_bin_path) / "run_dflowfm.sh"
+        script = "run_" + name + '.sh'
+        entry_point = Path(d3d_bin_path) / script
     else:
         raise OSError(f"Operating system '{os_name}' not supported")
     
-    if not dflowfm_entry_point.is_file():
+    if not entry_point.is_file():
         raise FileNotFoundError("Delft3D script could not be found at "
-                                f"{dflowfm_entry_point}")
+                                f"{entry_point}")
     
-    return dflowfm_entry_point
+    return entry_point
 
 
 def _pipe_reader(pipe: IO,
